67654edcbcf312b664660c4192bd74862ff61c00
EyalSegev123 <eyalsegev123@gmail.com>
2024-12-09 23:32:57 +0200

    First Day of Work - started the backend and frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomePage from "./pages/HomePage";
+
+import './styles/App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage /> {/* Rendering the HomePage component here */}
+    </div>
+  );
+}
+
+export default App;
